dff8551a08ebc0f23ed88b503612004c126cb564
GAURAV SHARMA <gsharma1154@gmail.com>
2023-12-19 06:13:07 +0530

    started studying security from nitin sir

---
+++ SpringSecurityIneuron/src/main/java/com/ineuron/SpringSecurityIneuronApplication.java
@@ -0,0 +1,13 @@
+package com.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityIneuronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityIneuronApplication.class, args);
+	}
+
+}
